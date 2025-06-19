448021d018d97ca92b1b9fd2051f17b99360f096
simon <sirnomidea@gmail.com>
2025-06-20 08:18:55 +1200

    update cname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://simontheteemo.github.io',
+  url: 'https://millerit.co.nz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/mcssite/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
